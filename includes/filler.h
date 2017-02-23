98a81aeb494f91fdcbbceddf6dc16c0bd7cd46cf
Denis ZHENG <dzheng@e2r11p16.42.fr>
2017-02-23 17:34:10 +0100

    header norm

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: dzheng <dzheng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 16:22:07 by dzheng            #+#    #+#             */
-/*   Updated: 2017/02/23 15:48:58 by dzheng           ###   ########.fr       */
+/*   Updated: 2017/02/23 17:33:47 by dzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,34 @@
 
 typedef struct			s_pc
 {
-	char				**shape; //Piece dans une str
-	int					pc_i; //TAILLE DE LA PIECE -Y
-	int					pc_j; //TAILLE DE LA PIECE -X
-	int					*stars_i; //COORDONNEES DE TOUTE LES ETOILES
-	int					*stars_j; //COORDONNEES DE TOUTE LES ETOILES
+	char				**shape;
+	int					pc_i;
+	int					pc_j;
+	int					*stars_i;
+	int					*stars_j;
 	int					cnt_stars;
 }						t_pc;
 
-typedef struct 			s_strat
+typedef struct			s_strat
 {
 	int					is_below;
 	int					can_up;
 }						t_strat;
 
-typedef struct 			s_coor
+typedef struct			s_coor
 {
-	char				id; //PLAYER
-	int					map_i; //TAILLE DE LA MAP -Y
-	int					map_j; //TAILLE DE LA MAP -X
-	char				**map; //MAP dans une str
-	int					gap; //DISTANCE ENTRE L'ADVERSAIRE
-	int					x; //coordonnees a print
-	int					y; //coordonnees a print
-	int					done; //Done with le parsing
+	char				id;
+	int					map_i;
+	int					map_j;
+	char				**map;
+	int					gap;
+	int					x;
+	int					y;
+	int					done;
 	t_pc				pc;
 	t_strat				strat;
 }						t_coor;
 
-//GET_DATA.C
 void					ft_get_map_coor(t_coor *coor, char *line);
 void					ft_get_pc_coor(t_coor *coor, char *line);
 char					*ft_get_map(char *line, char *map);
@@ -54,7 +53,6 @@ char					*ft_get_pc(char *line, char *pc);
 void					ft_check_line(t_coor *coor, char *line, char **pc,\
 						char **map);
 
-//GET_COORDINATE.C
 void					ft_get_coor_stars(t_coor *coor);
 int						ft_count_stars(t_coor *coor);
 void					ft_update_position_back(t_coor coor, int y_decal,\
@@ -62,11 +60,9 @@ void					ft_update_position_back(t_coor coor, int y_decal,\
 void					ft_update_position(t_coor coor, int y_decal,\
 						int x_decal);
 
-//FT_CHECKER.C
 int						ft_checker_map(t_coor *coor);
 int						ft_checker(t_coor *coor, int i, int j, int count);
 
-//ALGO.C
 void					ft_solve(t_coor *coor);
 void					ft_surround_him(t_coor *coor, int i, int j, int count);
 void					ft_fill_on_top(t_coor *coor);
